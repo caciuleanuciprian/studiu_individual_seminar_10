aaaefca931320552018943b777f5ee9c2cf6f2b4
Ciprian <56521866+caciuleanuciprian@users.noreply.github.com>
2020-11-11 21:34:42 +0200

    Add files via upload

---
+++ StudiuIndividualProblema10/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudiuIndividualProblema10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n;
+            float nrEl = 0;
+            float suma = 0;
+
+            Console.WriteLine("n: ");
+            n = int.Parse(Console.ReadLine());
+
+            float[] array = new float[n];
+
+
+            Console.WriteLine("Elementele vectorului: ");
+            for(int i= 0; i<array.Length; i++)
+            {
+                array[i] = float.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if(array[i] > 0)
+                {
+                    suma = suma + array[i];
+                    nrEl++;
+                }
+            }
+
+            Console.WriteLine("Media numerelor pozitive: " + (suma / nrEl));
+
+            Console.ReadLine();
+
+        }
+    }
+}
